f04982b1b9da96021eaf5166bc5cab56b8f74ab4
ikhwan000 <pratamaikhwan66@gmail.com>
2025-02-24 14:05:04 +0700

    Add files via upload

---
+++ models/koneksi.php
@@ -0,0 +1,18 @@
+<?php 
+    $dbhost="localhost";
+    $dbname="db_baru";
+    $dbuser="root";
+    $dbpass="";
+    
+    $dsn="mysql:host=$dbhost;dbname=$dbname";
+
+    try {
+        $dbh = new PDO($dsn, $dbuser, $dbpass);
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+    } catch (PDOException $e){
+        echo 'Connection Failed : '.$e->getMessage();
+    }
+
+    $koneksi = mysqli_connect("localhost","root","","db_baru",) or die ('database tidak terhubung');
+?>
